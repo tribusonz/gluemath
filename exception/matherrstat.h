965503a30fcf31173567813c83545defdea94aaa
Hironobu Inatsuka <103968337+tribusonz@users.noreply.github.com>
2022-04-22 20:55:08 +0900

    Add files via upload

---
+++ exception/matherrstat.h
@@ -0,0 +1,35 @@
+#ifndef MATHERRSTATUS_H
+#define MATHERRSTATUS_H
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+typedef enum MathErrStatus {
+	E_MATHNOERR = 0,   /* No error */
+	/* Domain Errors */
+	E_DOM,             /* Argument is out-of-domain */
+	E_DOMPOSARG,       /* Argument is positive */
+	E_DOMNEGARG,       /* Argument is negative */
+	E_DOMZERONEGARG,   /* Argument is negative or zero */
+	E_DOMZERO,         /* Argument is zero */
+	E_DOMLT,           /* Argument is less than something */
+	E_DOMLE,           /* Argument is less equal to something */
+	E_DOMGT,           /* Argument is greater than something */
+	E_DOMGE,           /* Argument is greater equal to something */
+	E_DOMNE,           /* Argument is not equal to something */
+	E_DOMINFARG,       /* Argument is infinite number */
+	E_DOMNANARG,       /* Argument is NaN */
+	E_DOMNOSOLVE,      /* The argument have not a solve */
+	/* Pole Errors */
+	E_RANGE,           /* Pole is out-of-range */
+	E_RANGEUNDERFLOW,  /* Pole is underflow */
+	E_RANGEOVERFLOW,   /* Pole is overflow */
+	E_RANGECONVERGE,   /* Can't converge */
+} matherrstatus_t;
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* MATHERRSTATUS_H */
