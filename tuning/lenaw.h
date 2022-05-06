d86134024a5920442eab89b113dc6470ef5bd2d6
Hironobu Inatsuka <103968337+tribusonz@users.noreply.github.com>
2022-05-06 11:15:31 +0900

    Add files via upload

---
+++ tuning/lenaw.h
@@ -0,0 +1,25 @@
+/*******************************************************************************
+	GLUE-Math Tuning: Length of Integration Coefficient Arrays
+
+	Author: Hironobu Inatsuka aka tribusonz
+	License: GPL + X11(MIT)
+	Reference:
+	  C-gengo ni yoru Hyoujun Algorithm Jiten - Haruhiko Okumura
+	  ISBN978-4-7741-9690-9
+*******************************************************************************/
+#ifndef GML_GML_LENGTH_OF_AW
+#define GML_GML_LENGTH_OF_AW
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#ifndef _LENAW
+# define _LENAW 8000
+#endif
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* GML_LENGTH_OF_AW */
