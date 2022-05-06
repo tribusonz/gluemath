14d357f26782647d1683140809fd0d1370297a47
Hironobu Inatsuka <103968337+tribusonz@users.noreply.github.com>
2022-05-06 09:22:49 +0900

    Add files via upload

---
+++ sys/quadrature/class.h
@@ -0,0 +1,20 @@
+/*******************************************************************************
+	GLUE-Math Quadrature primitive setting: Class
+	
+	Author: Hironobu Inatsuka aka tribusonz
+	License: GPL + X11(MIT)
+*******************************************************************************/
+#ifndef GML_QUADRATURE_PRIMITIVE_H
+#define GML_QUADRATURE_PRIMITIVE_H
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+typedef double integrand(double);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* GML_QUADRATURE_PRIMITIVE_H */
