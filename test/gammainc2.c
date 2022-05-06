e47dbf062f508ea92ffcc378753e20f661c2a70b
Hironobu Inatsuka <103968337+tribusonz@users.noreply.github.com>
2022-05-06 12:45:31 +0900

    Add files via upload

---
+++ test/gammainc2.c
@@ -0,0 +1,16 @@
+#include "../internal/ur/gammainc2.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main(void)
+{
+	double a, x;
+	
+	printf("a x ? ");  scanf("%lf%lf", &a, &x);
+	
+	printf("Gamma(%.15g, %.15g) = %.15g\n",
+	       a, x, gammainc2_core(a, x));
+	
+	return 0;
+}
