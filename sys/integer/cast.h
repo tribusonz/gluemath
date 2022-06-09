b6fa1e501786f676ede8ac48b5e2e6f6e4aef26b
Hironobu Inatsuka <103968337+tribusonz@users.noreply.github.com>
2022-06-09 13:32:17 +0900

    Update with fix typo

---
+++ sys/integer/cast.h
@@ -0,0 +1,754 @@
+/*******************************************************************************
+	GLUE-Math Integer Primitive Setting: Cast (Type Convertion)
+
+	Author: Hironobu Inatsuka aka tribusonz
+	License: GPL + X11(MIT)
+*******************************************************************************/
+#ifndef GML_INTEGER_PRIMITIVE_CAST_H
+#define GML_INTEGER_PRIMITIVE_CAST_H
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+// Declare Prototypes
+// Integers to char
+static inline char ctoc(char);
+static inline char uctoc(unsigned char);
+static inline char shttoc(short);
+static inline char ushttoc(unsigned short);
+static inline char itoc(int);
+static inline char uitoc(unsigned);
+static inline char ltoc(long);
+static inline char ultoc(unsigned long);
+static inline char lltoc(long long);
+static inline char ulltoc(unsigned long long);
+
+// Integers to unsigned char
+static inline unsigned char ctouc(char);
+static inline unsigned char uctouc(unsigned char);
+static inline unsigned char shttouc(short);
+static inline unsigned char ushttouc(unsigned short);
+static inline unsigned char itouc(int);
+static inline unsigned char uitouc(unsigned);
+static inline unsigned char ltouc(long);
+static inline unsigned char ultouc(unsigned long);
+static inline unsigned char lltouc(long long);
+static inline unsigned char ulltouc(unsigned long long);
+
+// Integers to short
+static inline short ctosht(char);
+static inline short uctosht(unsigned char);
+static inline short shttosht(short);
+static inline short ushttosht(unsigned short);
+static inline short itosht(int);
+static inline short uitosht(unsigned);
+static inline short ltosht(long);
+static inline short ultosht(unsigned long);
+static inline short lltosht(long long);
+static inline short ulltosht(unsigned long long);
+
+// Integers to unsigned short
+static inline unsigned short ctousht(char);
+static inline unsigned short uctousht(unsigned char);
+static inline unsigned short shttousht(short);
+static inline unsigned short ushttousht(unsigned short);
+static inline unsigned short itousht(int);
+static inline unsigned short uitousht(unsigned);
+static inline unsigned short ltousht(long);
+static inline unsigned short ultousht(unsigned long);
+static inline unsigned short lltousht(long long);
+static inline unsigned short ulltousht(unsigned long long);
+
+// Integers to int
+static inline int ctoi(char);
+static inline int uctoi(unsigned char);
+static inline int shttoi(short);
+static inline int ushttoi(unsigned short);
+static inline int itoi(int);
+static inline int uitoi(unsigned);
+static inline int ltoi(long);
+static inline int ultoi(unsigned long);
+static inline int lltoi(long long);
+static inline int ulltoi(unsigned long long);
+
+// Integers to unsigned int
+static inline unsigned ctoui(char);
+static inline unsigned uctoui(unsigned char);
+static inline unsigned shttoui(short);
+static inline unsigned ushttoui(unsigned short);
+static inline unsigned itoui(int);
+static inline unsigned uitoui(unsigned);
+static inline unsigned ltoui(long);
+static inline unsigned ultoui(unsigned long);
+static inline unsigned lltoui(long long);
+static inline unsigned ulltoui(unsigned long long);
+
+// Integers to long
+static inline long ctol(char);
+static inline long uctol(unsigned char);
+static inline long shttol(short);
+static inline long ushttol(unsigned short);
+static inline long itol(int);
+static inline long uitol(unsigned);
+static inline long ltol(long);
+static inline long ultol(unsigned long);
+static inline long lltol(long long);
+static inline long ulltol(unsigned long long);
+
+// Integers to unsigned long
+static inline unsigned long ctoul(char);
+static inline unsigned long uctoul(unsigned char);
+static inline unsigned long shttoul(short);
+static inline unsigned long ushttoul(unsigned short);
+static inline unsigned long itoul(int);
+static inline unsigned long uitoul(unsigned);
+static inline unsigned long ltoul(long);
+static inline unsigned long ultoul(unsigned long);
+static inline unsigned long lltoul(long long);
+static inline unsigned long ulltoul(unsigned long long);
+
+// Integers to long long
+static inline long long ctoll(char);
+static inline long long uctoll(unsigned char);
+static inline long long shttoll(short);
+static inline long long ushttoll(unsigned short);
+static inline long long itoll(int);
+static inline long long uitoll(unsigned);
+static inline long long ltoll(long);
+static inline long long ultoll(unsigned long);
+static inline long long lltoll(long long);
+static inline long long ulltoll(unsigned long long);
+
+// Integers to unsigned long long
+static inline unsigned long long ctoull(char);
+static inline unsigned long long uctoull(unsigned char);
+static inline unsigned long long shttoull(short);
+static inline unsigned long long ushttoull(unsigned short);
+static inline unsigned long long itoull(int);
+static inline unsigned long long uitoull(unsigned);
+static inline unsigned long long ltoull(long);
+static inline unsigned long long ultoull(unsigned long);
+static inline unsigned long long lltoull(long long);
+static inline unsigned long long ulltoull(unsigned long long);
+
+/******************************************************************************/
+
+// Integers to char
+static inline char
+ctoc(char x)
+{
+	return x;
+}
+
+static inline char
+uctoc(unsigned char x)
+{
+	return x;
+}
+
+static inline char
+shttoc(short x)
+{
+	return x;
+}
+
+static inline char
+ushttoc(unsigned short x)
+{
+	return x;
+}
+
+static inline char
+itoc(int x)
+{
+	return x;
+}
+
+static inline char
+uitoc(unsigned x)
+{
+	return x;
+}
+
+static inline char
+ltoc(long x)
+{
+	return x;
+}
+
+static inline char
+ultoc(unsigned long x)
+{
+	return x;
+}
+
+static inline char
+lltoc(long long x)
+{
+	return x;
+}
+
+static inline char
+ulltoc(unsigned long long x)
+{
+	return x;
+}
+
+// Integers to unsigned char
+static inline unsigned char
+ctouc(char x)
+{
+	return x;
+}
+
+static inline unsigned char
+uctouc(unsigned char x)
+{
+	return x;
+}
+
+static inline unsigned char
+shttouc(short x)
+{
+	return x;
+}
+
+static inline unsigned char
+ushttouc(unsigned short x)
+{
+	return x;
+}
+
+static inline unsigned char
+itouc(int x)
+{
+	return x;
+}
+
+static inline unsigned char
+uitouc(unsigned x)
+{
+	return x;
+}
+
+static inline unsigned char
+ltouc(long x)
+{
+	return x;
+}
+
+static inline unsigned char
+ultouc(unsigned long x)
+{
+	return x;
+}
+
+static inline unsigned char
+lltouc(long long x)
+{
+	return x;
+}
+
+static inline unsigned char
+ulltouc(unsigned long long x)
+{
+	return x;
+}
+
+// Integers to short
+static inline short
+ctosht(char x)
+{
+	return x;
+}
+
+static inline short
+uctosht(unsigned char x)
+{
+	return x;
+}
+
+static inline short
+shttosht(short x)
+{
+	return x;
+}
+
+static inline short
+ushttosht(unsigned short x)
+{
+	return x;
+}
+
+static inline short
+itosht(int x)
+{
+	return x;
+}
+
+static inline short
+uitosht(unsigned x)
+{
+	return x;
+}
+
+static inline short
+ltosht(long x)
+{
+	return x;
+}
+
+static inline short
+ultosht(unsigned long x)
+{
+	return x;
+}
+
+static inline short
+lltosht(long long x)
+{
+	return x;
+}
+
+static inline short
+ulltosht(unsigned long long x)
+{
+	return x;
+}
+
+// Integers to unsigned short
+static inline unsigned short
+ctousht(char x)
+{
+	return x;
+}
+
+static inline unsigned short
+uctousht(unsigned char x)
+{
+	return x;
+}
+
+static inline unsigned short
+shttousht(short x)
+{
+	return x;
+}
+
+static inline unsigned short
+ushttousht(unsigned short x)
+{
+	return x;
+}
+
+static inline unsigned short
+itousht(int x)
+{
+	return x;
+}
+
+static inline unsigned short
+uitousht(unsigned x)
+{
+	return x;
+}
+
+static inline unsigned short
+ltousht(long x)
+{
+	return x;
+}
+
+static inline unsigned short
+ultousht(unsigned long x)
+{
+	return x;
+}
+
+static inline unsigned short
+lltousht(long long x)
+{
+	return x;
+}
+
+static inline unsigned short
+ulltousht(unsigned long long x)
+{
+	return x;
+}
+
+// Integers to int
+static inline int
+ctoi(char x)
+{
+	return x;
+}
+
+static inline int
+uctoi(unsigned char x)
+{
+	return x;
+}
+
+static inline int
+shttoi(short x)
+{
+	return x;
+}
+
+static inline int
+ushttoi(unsigned short x)
+{
+	return x;
+}
+
+static inline int
+itoi(int x)
+{
+	return x;
+}
+
+static inline int
+uitoi(unsigned x)
+{
+	return x;
+}
+
+static inline int
+ltoi(long x)
+{
+	return x;
+}
+
+static inline int
+ultoi(unsigned long x)
+{
+	return x;
+}
+
+static inline int
+lltoi(long long x)
+{
+	return x;
+}
+
+static inline int
+ulltoi(unsigned long long x)
+{
+	return x;
+}
+
+// Integers to unsigned int
+static inline unsigned
+ctoui(char x)
+{
+	return x;
+}
+
+static inline unsigned
+uctoui(unsigned char x)
+{
+	return x;
+}
+
+static inline unsigned
+shttoui(short x)
+{
+	return x;
+}
+
+static inline unsigned
+ushttoui(unsigned short x)
+{
+	return x;
+}
+
+static inline unsigned
+itoui(int x)
+{
+	return x;
+}
+
+static inline unsigned
+uitoui(unsigned x)
+{
+	return x;
+}
+
+static inline unsigned
+ltoui(long x)
+{
+	return x;
+}
+
+static inline unsigned
+ultoui(unsigned long x)
+{
+	return x;
+}
+
+static inline unsigned
+lltoui(long long x)
+{
+	return x;
+}
+
+static inline unsigned
+ulltoui(unsigned long long x)
+{
+	return x;
+}
+
+// Integers to long
+static inline long
+ctol(char x)
+{
+	return x;
+}
+
+static inline long
+uctol(unsigned char x)
+{
+	return x;
+}
+
+static inline long
+shttol(short x)
+{
+	return x;
+}
+
+static inline long
+ushttol(unsigned short x)
+{
+	return x;
+}
+
+static inline long
+itol(int x)
+{
+	return x;
+}
+
+static inline long
+uitol(unsigned x)
+{
+	return x;
+}
+
+static inline long
+ltol(long x)
+{
+	return x;
+}
+
+static inline long
+ultol(unsigned long x)
+{
+	return x;
+}
+
+static inline long
+lltol(long long x)
+{
+	return x;
+}
+
+static inline long
+ulltol(unsigned long long x)
+{
+	return x;
+}
+
+// Integers to unsigned long
+static inline unsigned long
+ctoul(char x)
+{
+	return x;
+}
+
+static inline unsigned long
+uctoul(unsigned char x)
+{
+	return x;
+}
+
+static inline unsigned long
+shttoul(short x)
+{
+	return x;
+}
+
+static inline unsigned long
+ushttoul(unsigned short x)
+{
+	return x;
+}
+
+static inline unsigned long
+itoul(int x)
+{
+	return x;
+}
+
+static inline unsigned long
+uitoul(unsigned x)
+{
+	return x;
+}
+
+static inline unsigned long
+ltoul(long x)
+{
+	return x;
+}
+
+static inline unsigned long
+ultoul(unsigned long x)
+{
+	return x;
+}
+
+static inline unsigned long
+lltoul(long long x)
+{
+	return x;
+}
+
+static inline unsigned long
+ulltoul(unsigned long long x)
+{
+	return x;
+}
+
+
+// Integers to long long
+static inline long long
+ctoll(char x)
+{
+	return x;
+}
+
+static inline long long
+uctoll(unsigned char x)
+{
+	return x;
+}
+
+static inline long long
+shttoll(short x)
+{
+	return x;
+}
+
+static inline long long
+ushttoll(unsigned short x)
+{
+	return x;
+}
+
+static inline long long
+itoll(int x)
+{
+	return x;
+}
+
+static inline long long
+uitoll(unsigned x)
+{
+	return x;
+}
+
+static inline long long
+ltoll(long x)
+{
+	return x;
+}
+
+static inline long long
+ultoll(unsigned long x)
+{
+	return x;
+}
+
+static inline long long
+lltoll(long long x)
+{
+	return x;
+}
+
+static inline long long
+ulltoll(unsigned long long x)
+{
+	return x;
+}
+
+
+// Integers to unsigned long long
+static inline unsigned long long
+ctoull(char x)
+{
+	return x;
+}
+
+static inline unsigned long long
+uctoull(unsigned char x)
+{
+	return x;
+}
+
+static inline unsigned long long
+shttoull(short x)
+{
+	return x;
+}
+
+static inline unsigned long long
+ushttoull(unsigned short x)
+{
+	return x;
+}
+
+static inline unsigned long long
+itoull(int x)
+{
+	return x;
+}
+
+static inline unsigned long long
+uitoull(unsigned x)
+{
+	return x;
+}
+
+static inline unsigned long long
+ltoull(long x)
+{
+	return x;
+}
+
+static inline unsigned long long
+ultoull(unsigned long x)
+{
+	return x;
+}
+
+static inline unsigned long long
+lltoull(long long x)
+{
+	return x;
+}
+
+static inline unsigned long long
+ulltoull(unsigned long long x)
+{
+	return x;
+}
+
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* GML_INTEGER_PRIMITIVE_CAST_H */
